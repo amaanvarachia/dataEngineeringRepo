6ae365ce9653b46fb1e0bc838b9681b457f7b2a9
amaanvarachia <47464124+amaanvarachia@users.noreply.github.com>
2024-07-07 12:56:10 -0500

    Create request.py

---
+++ APIPullToDatabase/request.py
@@ -0,0 +1,17 @@
+#av
+import  requests
+from auth import headers
+import json
+
+class Request:
+    def __init__(self):
+        self._header = headers
+
+    @staticmethod
+    def get_content(url, param):
+        response = requests.get(url, headers=headers, params=param)
+        if response.status_code == 200:
+            return json.loads(response.content)
+        else:
+            print(f"Request completed but with Error. Response Code : {response.status_code}")
+            return None
